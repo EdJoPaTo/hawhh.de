22c62864d65e5755d79cca39dfe4959a551c2bc8
Edgar To <edjopato@gmail.com>
2016-03-06 00:17:18 +0100

    Wenn eine Suche eingegeben wurde zeige ein Element in der Liste dafür an

--- modules/hawhh/mainCtrl.js
+++ modules/hawhh/mainCtrl.js
@@ -1,9 +1,15 @@
 angular.module('hawhh')
-  .controller('mainCtrl', function($scope, $http) {
+  .controller('mainCtrl', function($scope, $http, duckDuckGoSearchLinkService) {
     $scope.q = "";
-    $scope.oftenlinks = [];
 
+    $scope.oftenlinks = [];
     $http.get("resources/oftenlinks.json").success(function(request) {
       $scope.oftenlinks = request;
+    });
+
+    $scope.currentsearch = { description: "DuckDuckGo Suche" };
+    $scope.$watch('q', function(newValue) {
+      $scope.currentsearch.name = $scope.q + " site:haw-hamburg.de";
+      $scope.currentsearch.url = "https:" + duckDuckGoSearchLinkService($scope.q + " site:haw-hamburg.de");
     });
   });
