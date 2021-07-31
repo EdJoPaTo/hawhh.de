4e3de4a15c18f5c31ae9b3959c82a1e21e11f356
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2021-07-31 07:01:00 +0200

    refactor(js): prefer includes over indexOf

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -4,7 +4,7 @@ filterInput.addEventListener("keyup", () => {
 	const criteria = filterInput.value.toLowerCase().trim();
 	const order = document.querySelectorAll(".list article");
 	order.forEach(data => {
-		if (data.textContent.toLowerCase().indexOf(criteria) >= 0) {
+		if (data.textContent.toLowerCase().includes(criteria)) {
 			data.style['display'] = null;
 		} else {
 			data.style['display'] = "none";
